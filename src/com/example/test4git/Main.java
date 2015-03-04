30e92c02524b7ed42801b1f3865627f573e32f55
chenwen3k <chenwen3k@gmail.com>
2015-03-04 10:41:23 +0800

    master

--- src/com/example/test4git/Main.java
+++ src/com/example/test4git/Main.java
@@ -1,7 +1,12 @@
 package com.example.test4git;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class Main extends Activity {
-
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
 }
